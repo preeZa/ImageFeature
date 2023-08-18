62c121de1ff8a79e983435329aae9694b7a51360
preeZa <preecha254513@gmail.com>
2023-08-18 15:03:00 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI , Request
+import cv2
+import numpy as np
+import base64
+from app.hog import gethog
+
+app = FastAPI()
+
+def read64(uri):
+    encoded_data = uri.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+    return img
+
+@app.get("/api/gethog")
+async def read_str(request : Request):
+    item = await request.json()
+    item_str = item['img']
+    img = read64(item_str)
+    hog = gethog(img)
+    return {"hog":hog.tolist()}
